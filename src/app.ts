8690ad28bf33eef353393a95abf63ccc751f65ed
LucasBeneditoCostaNeves <lucasbene03@gmail.com>
2023-06-14 21:03:13 -0300

    feat: verify token

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
+import "express-async-errors";
 import express, { Application } from "express";
 import { announcementRoutes } from "./routes/announcement.routes";
 import { handleAppErrorMiddleware } from "./middlewares/handleAppError.middlewares";
 
 const app: Application = express();
 app.use(express.json());
-app.use("/announcement", announcementRoutes);
 app.use(handleAppErrorMiddleware);
+app.use("/announcement", announcementRoutes);
 export default app;
