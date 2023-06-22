8b29504cfcd2e40c8c8ac901baf02a56a673a1f6
Gustavo <gustavo13gw@outlook.com>
2023-06-22 14:08:40 -0300

    feat: commit init

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,15 @@ import { announcementRoutes } from "./routes/announcement.routes";
 import { handleAppErrorMiddleware } from "./middlewares/handleAppError.middlewares";
 import { userRoutes } from "./routes/user.routes";
 import loginRoutes from "./routes/login.routes";
+import cors from "cors";
 
 const app: Application = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/announcements", announcementRoutes);
-app.use("/users", userRoutes)
-app.use("/login", loginRoutes)
+app.use("/users", userRoutes);
+app.use("/login", loginRoutes);
 
 app.use(handleAppErrorMiddleware);
 
